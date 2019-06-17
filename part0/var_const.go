3a142c644c8ac2fea01cf6a82a0ae74de45e5e71
paulf <bf_liu@outlook.com>
2019-06-17 18:23:27 +0800

    markdomn笔记

--- part0/var_const.go
+++ part0/var_const.go
@@ -1,11 +1,13 @@
 package part0
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 const final = 13213
 
 func Test() {
-
 	//var c1 = final
 	//var c2 = 1111
 
@@ -39,9 +41,48 @@ func Test() {
 	*p = 1000
 	fmt.Println(*p) //1000
 
+	fmt.Printf("最大公约数为%v\n", gys(16, 24))
+
+	fmt.Printf("斐波那契数列的第%v个数是%v\n", 15, fib(15))
+
+	s := "13213"
+	fmt.Println(strconv.Atoi(s))
+
+}
+
+/*
+计算斐波那契数列的第N个数
+*/
+func fib(n int) int {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		x, y = y, x+y
+	}
+	return x
+}
+
+/*计算最大公约数*/
+func gys(x, y int) int {
+	for y != 0 {
+		x, y = y, x%y
+	}
+	return x
 }
 
 func f() *int {
 	y := 1
 	return &y
+}
+
+var global *int
+
+func a() {
+	var x int
+	x = 1
+	global = &x
+}
+
+func b() {
+	x := new(int)
+	*x = 1
 }
